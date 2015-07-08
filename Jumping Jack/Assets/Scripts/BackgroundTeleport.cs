e57e5f3c7ff7201a616a48774b102330be165089
trolandus <johnnybe.beczcki@gmail.com>
2015-07-08 23:15:06 +0200

    Character is teleported when he reaches the edge of level

---
+++ Jumping Jack/Assets/Scripts/BackgroundTeleport.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundTeleport : MonoBehaviour {
+
+	public BackgroundTeleport output;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.collider.tag == "Player")
+			col.gameObject.transform.position = output.transform.position;
+	}
+}
