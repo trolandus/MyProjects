df048aee8e512025971ecaefb088c16fc608735a
trolandus <johnnybe.beczcki@gmail.com>
2015-07-15 19:41:33 +0200

    Zalążki kamery TPP

---
+++ Inżynier/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//OGRANICZENIA GÓRA-DÓŁ
+//PRZY CHODZENIU POSTAĆ IDZIE W KIERUNKU PATRZENIA KAMERY
+//PRZY STANIU KAMERA OBRACA SIĘ WOKÓŁ POSTACI
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject target;
+	public float distanceAway;
+	public float distanceUp;
+	public float smooth;
+
+	private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// LateUpdate is called once per frame after Update has finished
+	void LateUpdate () {
+		targetPosition = target.transform.position + target.transform.up * distanceUp - target.transform.forward * distanceAway;
+
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * smooth);
+
+		transform.LookAt (target.transform);
+	}
+}
---
+++ Inżynier/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movementSpeed = 10;
+	public float turningSpeed = 60;
+
+	private float horizontal;
+	private float vertical;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontal = Input.GetAxis ("Horizontal") * turningSpeed;
+		transform.Rotate (0, horizontal, 0);
+
+		vertical = Input.GetAxis ("Vertical") * movementSpeed;
+		transform.Translate (0, 0, vertical);
+	}
+}
