5db1b3da8717f338f5b17b98da6e2870cecbe233
trolandus <johnnybe.beczcki@gmail.com>
2015-09-02 20:41:55 +0200

    Pharmacy: Name of medicine appeares on mouse hover

--- Pharmacy/Assets/Scripts/Medicine.cs
+++ Pharmacy/Assets/Scripts/Medicine.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using UnityEditor;
+using UnityEngine.UI;
 
-public class Medicine : MonoBehaviour {
+public class Medicine : MonoBehaviour
+{
+    public string name;
+    public Text text;
 
     private Ray ray;
     private RaycastHit hit;
 
 	// Use this for initialization
-	void Start () 
-    {
-	
+	void Start ()
+	{
+	    text.text = name;
+	    text.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -25,6 +31,7 @@ public class Medicine : MonoBehaviour {
         {
             if (Input.GetMouseButton(0))
             {
+                text.enabled = false;
                 Vector3 vec3 = Input.mousePosition;
                 vec3.z = 10.0f;
                 vec3 = Camera.main.ScreenToWorldPoint(vec3);
@@ -38,10 +45,17 @@ public class Medicine : MonoBehaviour {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit))
         {
+            Vector2 pos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                text.GetComponentInParent<Canvas>().transform as RectTransform, Input.mousePosition,
+                text.GetComponentInParent<Canvas>().worldCamera, out pos);
+            text.transform.position = text.GetComponentInParent<Canvas>().transform.TransformPoint(pos);
+            text.enabled = true;
             return true;
         }
         else
         {
+            text.enabled = false;
             return false;
         }
     }
