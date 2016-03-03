eaeb661e39fbbaeb9a7986b176e4ad3600f55fe1
Jan Bełczącki <jan.belczacki@o2.pl>
2016-03-03 20:40:25 +0100

    Pierwsze próby w MonoGame.

---
+++ MonoGame/Game1/Game1/CameraController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    public class CameraController
+    {
+
+        #region variables
+        private Vector3 _camTarget;
+        private Vector3 _camPosition;
+        private Matrix _projectionMatrix;
+        private Matrix _viewMatrix;
+        private Matrix _worldMatrix;
+
+        private bool _orbit;
+        #endregion
+
+        #region properties
+        public Vector3 CamTarget
+        {
+            get { return _camTarget; }
+            set { _camTarget = value; }
+        }
+
+        public Vector3 CamPosition
+        {
+            get { return _camPosition; }
+            set { _camPosition = value; }
+        }
+
+        public Matrix ProjectionMatrix
+        {
+            get { return _projectionMatrix; }
+            set { _projectionMatrix = value; }
+        }
+
+        public Matrix ViewMatrix
+        {
+            get { return _viewMatrix; }
+            set { _viewMatrix = value; }
+        }
+
+        public Matrix WorldMatrix
+        {
+            get { return _worldMatrix; }
+            set { _worldMatrix = value; }
+        }
+
+        public bool Orbit
+        {
+            get { return _orbit; }
+            set { _orbit = value; }
+        }
+        #endregion
+
+        #region methods
+
+        public CameraController(Game game)
+        {
+            _camTarget = new Vector3(0, 0, 0);
+            _camPosition = new Vector3(0, 0, -100);
+            _projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.ToRadians(45f),
+                game.GraphicsDevice.DisplayMode.AspectRatio,
+                1, 1000);
+            _viewMatrix = Matrix.CreateLookAt(_camPosition, _camTarget,
+                Vector3.Up);
+            _worldMatrix = Matrix.CreateWorld(_camTarget, Vector3.Forward,
+                Vector3.Up);
+
+            _orbit = false;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            //Player controls for moving camera
+            CameraInput();
+
+            //Camera orbiting
+            if(_orbit)
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationY(
+                    MathHelper.ToRadians(1f));
+                _camPosition = Vector3.Transform(_camPosition, rotationMatrix);
+            }
+
+            _viewMatrix = Matrix.CreateLookAt(_camPosition, _camTarget, Vector3.Up);
+        }
+
+        void CameraInput()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                _camPosition.X += 1f;
+                _camTarget.X += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                _camPosition.X -= 1f;
+                _camTarget.X -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                _camPosition.Z += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                _camPosition.Z -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                _orbit = !_orbit;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ MonoGame/Game1/Game1/Game1.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        //Camera
+        CameraController camera;
+
+        //BasicEffect for rendering;
+        BasicEffect basicEffect;
+
+        //Geometric info for triangle
+        VertexPositionColor[] triangleVertices;
+        VertexBuffer vertexBuffer;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            //Add camera
+            camera = new CameraController(this);
+
+            //Basic effect
+            basicEffect = new BasicEffect(GraphicsDevice);
+            basicEffect.Alpha = 1f;
+
+            basicEffect.VertexColorEnabled = true;
+
+            //Lighting required information which VertexPositionColor does not have
+            basicEffect.LightingEnabled = false;
+
+            //Geometry of a simple triangle
+            triangleVertices = new VertexPositionColor[3];
+            triangleVertices[0] = new VertexPositionColor(new Vector3(0, 20, 0), Color.Red);
+            triangleVertices[1] = new VertexPositionColor(new Vector3(-20, -20, 0), Color.Green);
+            triangleVertices[2] = new VertexPositionColor(new Vector3(20, -20, 0), Color.Blue);
+
+            //Vertex buffer
+            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor),
+                3, BufferUsage.WriteOnly);
+            vertexBuffer.SetData<VertexPositionColor>(triangleVertices);
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            camera.Update(gameTime);
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            basicEffect.Projection = camera.ProjectionMatrix;
+            basicEffect.View = camera.ViewMatrix;
+            basicEffect.World = camera.WorldMatrix;
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            //Turning off culling
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach(EffectPass pass in basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 3);
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
